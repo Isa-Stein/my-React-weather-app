92a2213930f1cf25bd06a693bd83a7ef00829f29
Isabelle <isabellesteinhauslin@gmail.com>
2021-01-25 23:27:11 +0100

    Frame setup
    initial JSX coding

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import React from "react";
 import "./App.css";
 import Weather from "./Weather.js";
+import Developertag from "./Developertag.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-        <Weather city="Rome" />
-      </header>
+    <div className="app">
+      <div className="card">
+        <div className="container">
+          <div className="row">
+            <img
+              src="my-app/src/imgs/DayTime.png"
+              className="card-image-top"
+              alt="Time of Day Visual"
+            />
+          </div>
+          <div className="row">
+            <Weather />
+          </div>
+        </div>
+      </div>
+      <Developertag />
     </div>
   );
 }
---
+++ src/Developertag.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Developertag() {
+  return (
+    <p>
+      <a href="https://github.com/Isa-Stein/my-React-weather-app">
+        Open-sourced code
+      </a>{" "}
+      by Isabelle Steinhauslin
+    </p>
+  );
+}
